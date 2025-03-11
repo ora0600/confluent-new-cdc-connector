8fa2f233c56fc6305ac4c4ef882b3b8edbd61c11
C. Mützlitz <cmutzlitz@confluent.io>
2025-03-11 11:13:55 +0100

    Update 00_setup_cdc.sh

--- oraclexe21c/docker/scripts/00_setup_cdc.sh
+++ oraclexe21c/docker/scripts/00_setup_cdc.sh
@@ -8,6 +8,7 @@ echo "logging enabled"
 sqlplus sys/confluent123@XEPDB1 as sysdba @/opt/oracle/scripts/setup/02_create_user.sql
 sqlplus ordermgmt/kafka@XEPDB1 @/opt/oracle/scripts/setup/03_create_schema_datamodel.sql
 sqlplus ordermgmt/kafka@XEPDB1 @/opt/oracle/scripts/setup/04_load_data.sql
+sqlplus ordermgmt/kafka@XEPDB1 @/opt/oracle/scripts/setup/06_data_generator.sql
 # Create CDC User and align all roles
 # Users
 sqlplus sys/confluent123@XE as sysdba @/opt/oracle/scripts/setup/05_21c_create_user.sql
