fb033e6f765ef31e1cdd7fd6dfc0bc09a84f8c79
C. Mützlitz <cmutzlitz@confluent.io>
2025-03-11 11:06:22 +0100

    Update 05_21c_create_user.sql

--- oraclexe21c/docker/scripts/05_21c_create_user.sql
+++ oraclexe21c/docker/scripts/05_21c_create_user.sql
@@ -1,22 +1,22 @@
 -- Create Users
 ALTER SESSION SET CONTAINER = CDB$ROOT;
-CREATE USER c##ggadmin IDENTIFIED BY 'Confluent12!'
+CREATE USER c##ggadmin IDENTIFIED BY "Confluent12!"
   DEFAULT TABLESPACE users
   QUOTA UNLIMITED ON users
   CONTAINER=ALL;
 GRANT CREATE SESSION, SET CONTAINER TO c##ggadmin CONTAINER=ALL;
 BEGIN
    DBMS_XSTREAM_AUTH.GRANT_ADMIN_PRIVILEGE(
-      grantee                 => 'c##cggadmin',
+      grantee                 => 'C##GGADMIN',
       privilege_type          => 'CAPTURE',
       grant_select_privileges => TRUE,
       container               => 'ALL');
 END;
 /
 -- The following example creates a common user named c##cfltuser in a CDB
-CREATE USER c##cfltuser IDENTIFIED BY 'Confluent12!'
+CREATE USER c##cfltuser IDENTIFIED BY "Confluent12!"
   DEFAULT TABLESPACE users
-  QUOTA UNLIMITED ON user
+  QUOTA UNLIMITED ON users
   CONTAINER=ALL;
 GRANT CREATE SESSION, SET CONTAINER TO c##cfltuser CONTAINER=ALL;
 GRANT SELECT_CATALOG_ROLE TO c##cfltuser CONTAINER=ALL;
