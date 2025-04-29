cf1369a79df116f8dc1b4c84681532ab66023051
Carsten Mützlitz <cmutzlitz@confluent.io>
2025-04-29 17:29:01 +0200

    Update 00_create_client.properties.sh

--- oraclexe21c/00_create_client.properties.sh
+++ oraclexe21c/00_create_client.properties.sh
@@ -8,6 +8,11 @@ echo $BASEDIR
 export pubip=$1
 export oracle_host=$1
 
+#export pubip=$(echo -e "$(terraform output -raw A01_PUBLICIP)")
+#echo $pubip
+#export oracle_host=$(echo -e "$(terraform output -raw A02_ORACLESERVERNAME)")
+#echo $oracle_host
+
 source .aws_env
 
 # Replace Parameters cdc_ccloud.json.template
@@ -15,6 +20,16 @@ source .aws_env
 broker="${bootstrap:11}"
 
 cd ../cdc-connector/
+
+# Generate .ccloud_env for Oracle21c CDC Ccloud connector setup
+echo "export TF_VAR_confluent_cloud_api_key="\"${confluent_cloud_api_key}\""
+export TF_VAR_confluent_cloud_api_secret="\"${confluent_cloud_api_secret}\""
+export TF_VAR_envid="\"${envid}\""
+export TF_VAR_clusterid="\"${clusterid}\""
+export TF_VAR_said="\"${said}\""
+export TF_VAR_oracle_host="\"${oracle_host}\""" > .ccloud_env
+
+
 # make a copy from template
 cp cdc_ccloud.json.template cdc_ccloud.json
 SCRIPT1="sed -i -e 's|##oracle_host##|$oracle_host|g' cdc_ccloud.json;"
@@ -58,5 +73,3 @@ bash -c "$SSCRIPT6"
 bash -c "$SSCRIPT6"
 
 mv docker-compose-cdc-ccloud_new.yml-e docker-compose-cdc-ccloud_new.yml
-
-
