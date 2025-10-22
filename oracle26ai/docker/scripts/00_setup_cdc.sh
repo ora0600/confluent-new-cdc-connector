5d3761e22de5342ddc6e57843c0cbde16f353141
Carsten Mützlitz <cmutzlitz@confluent.io>
2025-10-22 16:40:51 +0200

    Add data generator script execution to setup

--- oracle26ai/docker/scripts/00_setup_cdc.sh
+++ oracle26ai/docker/scripts/00_setup_cdc.sh
@@ -8,6 +8,7 @@ echo "logging enabled"
 sqlplus sys/confluent123@FREEPDB1 as sysdba @/opt/oracle/scripts/setup/02_create_user.sql
 sqlplus ordermgmt/kafka@FREEPDB1 @/opt/oracle/scripts/setup/03_create_schema_datamodel.sql
 sqlplus ordermgmt/kafka@FREEPDB1 @/opt/oracle/scripts/setup/04_load_data.sql
+sqlplus ordermgmt/kafka@FREEPDB1 @/opt/oracle/scripts/setup/06_data_generator.sql
 # Create CDC User and align all roles
 sqlplus sys/confluent123@FREE as sysdba @/opt/oracle/scripts/setup/05_26ai_create_user.sql
 sqlplus sys/confluent123@FREEPDB1 as sysdba @/opt/oracle/scripts/setup/05_26ai_privs.sql
