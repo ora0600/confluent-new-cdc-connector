a2289f30761311138feae456fae6d0eda262b055
Carsten Mützlitz <cmutzlitz@confluent.io>
2025-11-05 14:17:55 +0100

    Update 02_create_user.sql

--- oraclexe21c/docker/scripts/02_create_user.sql
+++ oraclexe21c/docker/scripts/02_create_user.sql
@@ -12,8 +12,10 @@ Create user ordermgmt identified by kafka;
 grant RESOURCE to ordermgmt;
 grant create session to ordermgmt;
 grant execute on DBMS_LOCK to ordermgmt;
+grant execute on sys.dbms_crypto to ORDERMGMT;
 
 -- give right to write data to tablespace
 alter user ordermgmt quota 50m on USERS;
 
 exit;
+
